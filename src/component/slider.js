631780018da154e677a8f9001242a81f53096bbe
sidduajay123 <sidduajay123@gmail.com>
2023-02-14 21:22:22 +0530

    college detail page

--- src/component/slider.js
+++ src/component/slider.js
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-<<<<<<< HEAD
 import img_logo from '../img/sk.jpeg';
-=======
->>>>>>> 6b108303e0fcc1f469d976a30246883fb9d06832
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import Leftpanel from "./leftpanel";
@@ -167,11 +164,7 @@ function Slider() {
                                                                 <span className="fa fa-star checked"></span>
                                                                 <span className="fa fa-star"></span>
                                                                 <span className="fa fa-star"></span></p>
-<<<<<<< HEAD
-                                                          
-=======
                                                                 <p>Course Duration : {res.duration}</p>
->>>>>>> 6b108303e0fcc1f469d976a30246883fb9d06832
                                                         </li>
                                                         <li className="location">
                                                             <p>Location: <b>Kompalli</b></p>
