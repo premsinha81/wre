9af39f261fccee9f763327fed1aea580130bdd85
prem sinha <premsinha77@gmail.com>
2023-05-31 13:05:05 +0530

    code merge

--- src/component/common/footer.js
+++ src/component/common/footer.js
@@ -1,98 +1,4 @@
 export default function Footer() {
-<<<<<<< HEAD
-    return (
-        <>
-            <footer class="footerSection">
-                <div class="container">
-                    <div class="row">
-                    <div class="col-lg-4">
-                            <div class="footerBox footer1">
-                                <div class="footerTitle">
-                                    <h3>About Us </h3>
-                                </div>
-                                <div class="footerBody">
-                                    <ul>
-                                        <li className="about">PTTI was founded on this exact philosophy 10 years ago. PTTI was formed by a Drexel University graduate who believes in our youth to receive a technology-driven education so that they can secure...</li>
-                                        <li><div class="followUs1">
-                        <ul class="solialLink">
-                           <li><a href="" class="socialLinkItem"><i class="fa fa-facebook"></i></a></li>
-                           <li><a href="" class="socialLinkItem"><i class="fa fa-twitter"></i></a></li>
-                           <li><a href="" class="socialLinkItem"><i class="fa fa-youtube-play"></i></a></li>
-                           <li><a href="" class="socialLinkItem"><i class="fa fa-instagram"></i></a></li>
-                        </ul>
-                     </div></li>
-                                    </ul></div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3">
-                            <div class="footerBox footer1">
-                                <div class="footerTitle">
-                                    <h3>Services</h3>
-                                </div>
-                                <div class="footerBody">
-                                    <ul>
-                                        <li><a href="">Find a Program</a></li>
-                                        <li><a href=""> Job Search</a></li>
-                                        <li><a href="">Community</a></li>
-                                        <li><a href=""> Resources</a></li>
-                                    </ul></div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3">
-                            <div class="footerBox footer1">
-                                <div class="footerTitle">
-                                    <h3>Important Links</h3>
-                                </div>
-                                <div class="footerBody">
-                                    <ul>
-                                        <li><a href="">About WRE</a></li>
-                                        <li><a href=""> Contributors  &amp; Experts</a></li>
-                                      
-                                        <li><a href="">FAQ's</a></li>
-                                        <li><a href="">Pricing</a></li>
-                                    </ul></div>
-                            </div>
-                        </div>
-                       
-                        <div class="col-lg-2">
-                            <div class="footerBox footer1">
-                                <div class="footerTitle1">
-                                    <h3>Contact Details</h3>
-                                </div>
-                                <div class="footerBody">
-                                <ul >
-                           <li><a href="#" class="socialLinkItem1"><i class="fa fa-phone"></i> +91-89898988988</a></li>
-                           <li><a href="#" class="socialLinkItem1"><i class="fa fa-envelope"></i> info@wre.com</a></li>
-                        </ul>
-                        </div>
-                        
-                        
-                         
-                            </div>
-
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="subscirpSection">
-                            <h1 class="subscribeTitle">Stay Connected</h1>
-                                <form action="" class="subscribeForm">
-                                    <div class="inputField">
-                                        <input type="text" name="email" placeholder="e-Mail ID" required=""/>
-                                            <button type="submit" class="subscribeBtn">Subscribe</button>
-                                    </div>
-                                </form>
-                                <div class="termsLink"><ul class="tandcLink"><li><a href="" class=""></a></li><li><a href="" class=""></a></li></ul></div>
-                                <div class="termsLink">
-                                    <ul class="tandcLink"> 
-                                        <li><a href="" class="">Privacy Policy</a></li>
-                                        <li><a href="" class="">Terms of use</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-=======
   return (
     <>
       <footer class="footerSection">
@@ -102,7 +8,6 @@ export default function Footer() {
               <div class="footerBox footer1">
                 <div class="footerTitle">
                   <h3>WRE </h3>
->>>>>>> 902a808ab27ccf7a6bbabb7bd909af91c97fb3d7
                 </div>
                 <div class="footerBody">
                   <ul>
