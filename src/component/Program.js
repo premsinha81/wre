ca10936dd20ba569c81b1f31332fa7cad4e4bc18
Riyadubey1 <riyaupa123@gmail.com>
2023-12-05 17:53:20 +0530

    changes

---
+++ src/component/Program.js
@@ -0,0 +1,17 @@
+import Slider from "./slider"
+import Popup from "./Popup"
+
+
+function Program() {
+    return (
+        
+        <>
+         
+            <Slider></Slider>
+            <Popup></Popup>
+            
+        </>
+    )
+}
+
+export default Program;
